fb29b8f4d03001df1e29eef0839ddf65da54f70c
Gabriele Oliaro <goliaro@cs.cmu.edu>
2025-02-12 05:30:35 +0000

    update

--- .github/workflows/helpers/install_nccl.sh
+++ .github/workflows/helpers/install_nccl.sh
@@ -7,8 +7,9 @@ cd "${BASH_SOURCE[0]%/*}"
 
 ubuntu_version=$(lsb_release -rs)
 ubuntu_version=${ubuntu_version//./}
-wget "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/x86_64/cuda-keyring_1.0-1_all.deb"
-DEBIAN_FRONTEND=noninteractive dpkg -i cuda-keyring_1.0-1_all.deb
-DEBIAN_FRONTEND=noninteractive apt-get update -y --allow-change-held-packages
-rm -f cuda-keyring_1.0-1_all.deb
-DEBIAN_FRONTEND=noninteractive apt install -y --allow-change-held-packages libnccl2 libnccl-dev
+wget -c -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ubuntu_version}/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update -y
+rm -f cuda-keyring_1.1-1_all.deb
+sudo apt-get update -y --allow-change-held-packages
+sudo apt install -y --allow-change-held-packages libnccl2 libnccl-dev
