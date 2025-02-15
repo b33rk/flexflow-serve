67ff7264c0e99150fafdf05c530dc128a03d56c0
Gabriele Oliaro <goliaro@cs.cmu.edu>
2025-02-15 04:54:44 +0000

    fix

--- src/parallel_ops/kernels/allreduce_kernels.cu
+++ src/parallel_ops/kernels/allreduce_kernels.cu
@@ -136,12 +136,12 @@ void inference_kernel_wrapper(AllReduceMeta *m,
   ncclComm_t ncclComm = m->handle.ncclComm;
   DataType dtype = input.data_type;
 
-  tensorrt_llm::AllReduceStrategyType strategy =
-      tensorrt_llm::SelectImplementation(
-          num_elements * ((get_bits(dtype) + 7) / 8), num_devices);
+  // tensorrt_llm::AllReduceStrategyType strategy =
+  //     tensorrt_llm::SelectImplementation(
+  //         num_elements * ((get_bits(dtype) + 7) / 8), num_devices);
 
-  if (strategy == tensorrt_llm::AllReduceStrategyType::RING ||
-      !CanApplyCustomAllReduce(num_elements, dtype)) {
+  // if (strategy == tensorrt_llm::AllReduceStrategyType::RING ||
+  //     !CanApplyCustomAllReduce(num_elements, dtype)) {
     // Dispatch to nccl AllReduce if the customized all-reduce cannot apply.
     ncclDataType_t nccl_data_type = ff_to_nccl_datatype(dtype);
     checkNCCL(ncclAllReduce(input.ptr,
@@ -151,42 +151,42 @@ void inference_kernel_wrapper(AllReduceMeta *m,
                             ncclSum,
                             ncclComm,
                             stream));
-    return;
-  }
+  //   return;
+  // }
 
-  // Initialize the all-reduce kernel arguments.
-  tensorrt_llm::AllReduceParams params;
-  params.ranks_per_node = num_devices;
-  params.rank = device_id;
-  params.local_rank = device_id;
-  CommunicationBuffer *comm_buffer =
-      get_or_create_comm_buffer(m,
-                                num_devices,
-                                device_id,
-                                ncclComm,
-                                const_cast<void *>(input.ptr),
-                                stream);
-  params.barrier_flag = ++(*comm_buffer->barrier_flag);
-  for (int i = 0; i < num_devices; ++i) {
-    params.peer_comm_buffer_ptrs[i] = comm_buffer->comm_ptrs[i];
-  }
-  for (int i = 0; i < num_devices; ++i) {
-    params.peer_barrier_ptrs_in[i] =
-        reinterpret_cast<uint32_t *>(comm_buffer->barrier_in[i]);
-  }
-  for (int i = 0; i < num_devices; ++i) {
-    params.peer_barrier_ptrs_out[i] =
-        reinterpret_cast<uint32_t *>(comm_buffer->barrier_out[i]);
-  }
+  // // Initialize the all-reduce kernel arguments.
+  // tensorrt_llm::AllReduceParams params;
+  // params.ranks_per_node = num_devices;
+  // params.rank = device_id;
+  // params.local_rank = device_id;
+  // CommunicationBuffer *comm_buffer =
+  //     get_or_create_comm_buffer(m,
+  //                               num_devices,
+  //                               device_id,
+  //                               ncclComm,
+  //                               const_cast<void *>(input.ptr),
+  //                               stream);
+  // params.barrier_flag = ++(*comm_buffer->barrier_flag);
+  // for (int i = 0; i < num_devices; ++i) {
+  //   params.peer_comm_buffer_ptrs[i] = comm_buffer->comm_ptrs[i];
+  // }
+  // for (int i = 0; i < num_devices; ++i) {
+  //   params.peer_barrier_ptrs_in[i] =
+  //       reinterpret_cast<uint32_t *>(comm_buffer->barrier_in[i]);
+  // }
+  // for (int i = 0; i < num_devices; ++i) {
+  //   params.peer_barrier_ptrs_out[i] =
+  //       reinterpret_cast<uint32_t *>(comm_buffer->barrier_out[i]);
+  // }
 
-  if (!CanApplyTwoShotAllReduce(num_elements, dtype, num_devices)) {
-    // Two-shot all-reduce does not support this case.
-    // So we fallback to the one-shot strategy.
-    strategy = tensorrt_llm::AllReduceStrategyType::ONESHOT;
-  }
+  // if (!CanApplyTwoShotAllReduce(num_elements, dtype, num_devices)) {
+  //   // Two-shot all-reduce does not support this case.
+  //   // So we fallback to the one-shot strategy.
+  //   strategy = tensorrt_llm::AllReduceStrategyType::ONESHOT;
+  // }
 
-  tensorrt_llm::customAllReduce(
-      params, output.ptr, num_elements, dtype, strategy, stream);
+  // tensorrt_llm::customAllReduce(
+  //     params, output.ptr, num_elements, dtype, strategy, stream);
 }
 
 void forward_kernel_wrapper(AllReduceMeta const *m,
